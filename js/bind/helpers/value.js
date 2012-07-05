b3978f2a0fddd74b32d8c03a01cbae9bb9ee400b
mjeffery <mjeffery@handlebind.js>
2012-07-05 11:21:35 -0400

    read-only observable bindings work.  computed does not resolve correctly

--- js/bind/helpers/value.js
+++ js/bind/helpers/value.js
@@ -2,8 +2,8 @@ define(
 ['lib/underscore', 'lib/handlebars', 'observe/observable', 'bind/util'], 
 function(_, Handlebars, observable, util) {
 	Handlebars.registerHelper('value', function(context, options) {
-		return util.wrap(context, function(context, options) {
-			return context.toString();
+		return util.wrap(context, function(value, options) {
+			return !!value ? value.toString() : "";
 		})
 	})
 });
