b4efaf4c93fe2483ec652bcd87d48019d23870c8
Derek Nelson <dereknelson01@gmail.com>
2012-07-05 16:37:26 -0400

    Added methods to ObservableArray

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ require.config({
 			exports: 'Handlebars'
 		}
 	}
-})
+});
 
 require(['lib/jquery', 'bind/View', 'hb'], function($, View, hb) {
 	$(document).ready(function() {
@@ -21,23 +21,11 @@ require(['lib/jquery', 'bind/View', 'hb'], function($, View, hb) {
 				this.firstName = hb.observable("tim");
 				this.lastName =  hb.observable("schaffer");
 				this.fullName =  hb.computed(function() {
-					return this.firstName() + ' ' + this.lastName();
+					return this.firstName + ' ' + this.lastName;
 				}, this);
 			}
-		
-		var viewModel = new ViewModel();
-		var view = new View('test-template', viewModel);
-		view.appendTo('#app-container');
-		
-		$('#toggle-names').on('click', function() {
-			var first = viewModel.firstName();
-			if(first === 'belinda')
-				viewModel.firstName('tim');
-			else
-				viewModel.firstName('belinda');
-				
-			console.log('first: ' + viewModel.firstName());
-			console.log('name: ' + viewModel.fullName());
-		})
+			
+		var view = new View('test-template', new ViewModel());
+		view.appendTo('body');
 	});
 })
